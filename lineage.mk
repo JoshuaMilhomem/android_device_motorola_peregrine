c9d1309d37413572e93d360270651d491cb78a14
Gaurav <allstargaurav@gmail.com>
2018-01-16 22:01:06 +0530

    Define Bootanimation Resolution

--- lineage.mk
+++ lineage.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := lineage_peregrine
 
